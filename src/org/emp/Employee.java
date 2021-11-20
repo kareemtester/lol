1637201682ade3e31780183745c610f07928a8aa
kareemtester <kareemsheriff17@gmail.com>
2021-11-20 13:03:19 +0530

    lol

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,38 @@
+package org.emp;
+
+public class Employee {
+	private void EmployeeId() {
+		System.out.println("Employee ID is : 3244");
+	}
+	
+	private void EmployeeName() {
+		System.out.println("Employee Name is kareem");
+	}
+	private void EmpDob() {
+		System.out.println("Employee DOB is 24-02-1997");
+	}
+	private void EmpPhone() {
+		System.out.println("Employee contact number is : 9566126981");
+    }
+	private void EmployeeEmail() {
+		System.out.println("Employee Email is : kareemsheriff15@gmail.com");
+
+	}
+	private void EmployeeAddress() {
+		System.out.println("Employee Address is : Perambur");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		Employee a = new Employee();
+		a.EmployeeName();
+		a.EmployeeId();
+		a.EmpDob();
+		a.EmpPhone();
+		a.EmployeeEmail();
+		a.EmployeeAddress();
+	}
+	
+
+}
